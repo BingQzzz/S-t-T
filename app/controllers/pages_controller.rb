a7de3a7046128c5c756f4d9bc1489b3f41978e13
BingQ <bingbing13@163.com>
2020-05-22 00:42:32 -0700

    bisic structure

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  # speech to text
+  def stt
+  end
+
+  # text to speech
+  def tss
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  get 'home' =>"pages#home", as: "home"
+
+  # speech to text
+  get 'stt' =>"pages#stt", as: "stt"
+  # text to speech
+  get 'tts' =>"pages#tts", as: "tts"
 end
